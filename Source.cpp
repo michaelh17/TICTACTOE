2a73c172ea72844a3560840e5b89c85198362bd5
unknown <michaelxu17@gmail.com>
2020-11-01 23:59:41 +0700

    Add tictactoeboard function and marker

--- Source.cpp
+++ Source.cpp
@@ -21,7 +21,7 @@ void rules() { //Function Rules
 		}
 		else if (sel_main == "n") {
 			system("CLS");
-			cout << "\nAlright , See you next time! " << endl;
+			cout << "\n\aAlright , See you next time! " << endl;
 			exit(1);
 		}
 		else {
@@ -29,6 +29,18 @@ void rules() { //Function Rules
 		}
 	} while (sel_main != "y");
 }
+
+
+void tictactoeboard(int b,int k, int tictactoe[3][3]) {
+	for (b = 0; b < 3; b++) {
+		for (k = 0; k < 3; k++) {
+			cout << "  " << tictactoe[b][k] << "  |";
+		}
+		cout << "\n-----+-----+------";
+		cout << endl;
+	}
+}
+
 
 int func_turn(int turn) { //function giliran
 	int tpl;
@@ -42,9 +54,11 @@ int main() {
 	int k = 0;
 	int turn = 0;
 	int choose, line;
-	int col;
+	int col = 0;
 	char nm1[20];
 	char nm2[20];
+	char marker;
+	char symbol[2] = { 'X','O' };
 	int tictactoe[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
 
 	rules(); // pemanggilan function Rules
@@ -55,43 +69,27 @@ int main() {
 	cout << "\nInput Player 2 Name : ";
 	cin >> nm2;
 
-	for (b = 0; b < 3; b++) {
-		for (k = 0; k < 3; k++) {
-			cout << "  " << tictactoe[b][k] << "  |";
-		}
-		cout << "\n-----+-----+------";
-		cout << endl;
-	}
+	tictactoeboard(b,k,tictactoe); //pemanggilan function tictactoeboard
 
-	for (turn = 0; turn < 9; turn++) {
+	for (turn = 0; turn < 9; turn++) { //looping maksimal selection
 		func_turn(turn); //pemanggilan function giliran
+
 		if (func_turn(turn) == 1) {
 			cout << "\nIt's " << nm1 << " Turn!";
+			marker = symbol[0];
 		}
 
 		if (func_turn(turn) == 2) {
 			cout << "\nIt's " << nm2 << " Turn!";
+			marker = symbol[1];
 		}
 
+		cout << "\nInsert box number you choose : ";
+		cin >> choose;
 
-		do {
-			cout << "\nEnter the box number you choose : ";
-			cin >> choose;
-
-			if (choose >= 1 && choose <= 3) {
-				line = 0;
-				col = col - 1;
-			}
-			else if (choose >= 4 && choose <= 6) {
-				line = 1;
-				col = col - 4;
-			}
-			else {
-				line = 2;
-				col = col - 7;
-			}
-		} while (col < 1 || col >9 || tictactoe[line][col] == 'x' || tictactoe[line][col] == 'o');
-
+		if (choose == 1 && tictactoe[0][0] == 1)
+			tictactoe[0][0] = marker;
+		
 	}
 	return 0;
 }	
